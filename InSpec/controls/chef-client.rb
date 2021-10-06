19430deab0ebcc9c8d9054ab3b1934596c81f714
Lynn Caldwell <84191937+caldlyn@users.noreply.github.com>
2021-10-06 14:54:14 -0500

    Update chef-client.rb

--- InSpec/controls/chef-client.rb
+++ InSpec/controls/chef-client.rb
@@ -4,7 +4,6 @@ control 'chef-client-1.0' do
     title 'Chef-client version'
     desc 'Chef-client version'
     describe command('chef-client --version') do
-      its('stdout') { should match (/Chef: 1[7]/) }
+      its('stdout') { should match (/Chef Infra Client: 17.6.15/)}
     end
 end
-
